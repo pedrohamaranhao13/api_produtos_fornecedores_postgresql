b306cf9b03d0b09735cade0328fbddda2008a8ee
pedrohamaranhao13 <pedrinhoelloco@outlook.com.br>
2024-09-17 15:45:37 -0300

    feature: Métodos do FornecedorController.

--- src/main/java/br/com/phamtecnologia/api_produtos/controllers/FornecedoresController.java
+++ src/main/java/br/com/phamtecnologia/api_produtos/controllers/FornecedoresController.java
@@ -1,5 +1,8 @@
 package br.com.phamtecnologia.api_produtos.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -46,23 +49,72 @@ public class FornecedoresController {
     }
 
     @PutMapping
-    public void put(@RequestBody FornecedorPutDto dto) {
-        //TODO
+    public ResponseEntity<String> put(@RequestBody FornecedorPutDto dto) {
+        
+        try {
+            
+            Optional<Fornecedor> optional = fornecedorRepository.findById(dto.getIdFornecedor());
+
+            if (optional.isPresent()) {
+                
+                Fornecedor fornecedor = optional.get();
+                fornecedor.setNome(dto.getNome());
+                fornecedor.setCnpj(dto.getCnpj());
+                fornecedor.setTelefone(dto.getTelefone());
+
+                fornecedorRepository.save(fornecedor);
+
+                return ResponseEntity.status(200).body("Fornecedor atualizado com sucesso.");
+            } else {
+                return ResponseEntity.status(404).body("Fonecedor não encontrado.");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Não foi possível atualizar o fornecedor. " + e.getMessage());
+        }
     }
 
     @DeleteMapping("{idFornecedor}")
-    public void delete(@PathVariable("idFornecedor") Integer idFornecedor) {
-        //TODO
+    public ResponseEntity<String> delete(@PathVariable("idFornecedor") Integer idFornecedor) {
+        try {
+            Optional<Fornecedor> optional = fornecedorRepository.findById(idFornecedor);
+
+            if (optional.isPresent()) {
+                Fornecedor fornecedor = optional.get();
+                fornecedorRepository.delete(fornecedor);
+
+                return ResponseEntity.status(200).body("Fornecedor excluído com sucesso.");
+            } else {
+                return ResponseEntity.status(404).body("Fornecedor não encontrado.");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Não foi possível excluir fonecedor. " + e.getMessage());
+        }
     }
 
     @GetMapping
-    public void getAll() {
-        //TODO
+    public ResponseEntity<List<Fornecedor>> getAll() {
+        try {
+            List<Fornecedor> fornecedores = fornecedorRepository.findAll();
+            return ResponseEntity.status(200).body(fornecedores);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(null);
+        }
     }
 
     @GetMapping("{idFornecedor}")
-    public void getById(@PathVariable("idFornecedor") Integer idFornecedor) {
-        //TODO
+    public ResponseEntity<Fornecedor> getById(@PathVariable("idFornecedor") Integer idFornecedor) {
+        try {
+            Optional<Fornecedor> optional =fornecedorRepository.findById(idFornecedor);
+
+            if (optional.isPresent()) {
+                Fornecedor fornecedor = optional.get();
+                return ResponseEntity.status(200).body(fornecedor);
+            } else {
+                return ResponseEntity.status(204).body(null);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(null);
+        }
     }
 
 }
